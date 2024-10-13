ed411315a644570da2f2434f8beedeb4daf54949
MazharZiadeh <147834037+MazharZiadeh@users.noreply.github.com>
2024-10-13 10:23:56 +0300

    simulate world

---
+++ EVO/simulate.py
@@ -0,0 +1,14 @@
+import pybullet as p
+import time
+
+# Connect to PyBullet GUI
+physicsClient = p.connect(p.GUI)
+
+# Simulate the world for 1000 steps
+for i in range(1000):
+    p.stepSimulation()
+    print(i)
+    time.sleep(1/60)  # Sleep for 1/60th of a second
+
+# Disconnect from PyBullet
+p.disconnect()
